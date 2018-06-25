8d276ee506d855937f265b20a38e19e3e70b0f39
Kades420 <kades59@gmail.com>
2018-06-25 15:12:35 +0300

    First commit

---
+++ soap client and server/soap client/src/main/java/com/ics/soap/DemoApplication.java
@@ -0,0 +1,35 @@
+package com.ics.soap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner lookup(CountryClient countryClient) {
+        return (args) -> {
+            String name = "Poland";
+            if (args.length > 0) {
+                name = args[0];
+            }
+
+            countries.wsdl.GetCountryResponse response = countryClient.getCountry(name);
+            countries.wsdl.Country country = response.getCountry();
+
+            String result = country.getName()     + "\n" +
+                    country.getCapital()  + "\n" +
+                    country.getCurrency() + "\n" +
+                    country.getPopulation();
+
+            System.out.println(result);
+        };
+    }
+}
+
